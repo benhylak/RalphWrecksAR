0a5d50550ac742ba1baf546f778a2f22a3cfa8e9
Elizabeth Kirschner <eekirschner@wpi.edu>
2018-12-14 05:31:56 -0500

    Finished walking stick implementation, added sound effects, added toggle for mono/stereo disability

---
+++ Assets/Scripts/ToggleScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ToggleScript : MonoBehaviour {
+
+	public Toggle m_Toggle;
+	public Text m_Text;
+
+	// Use this for initialization
+	 void Start()
+    {
+        //Fetch the Toggle GameObject
+        m_Toggle = GetComponent<Toggle>();
+        //Add listener for when the state of the Toggle changes, to take action
+        m_Toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(m_Toggle);
+        });
+
+        //Initialise the Text to say the first state of the Toggle
+        m_Text.text = "First Value : " + m_Toggle.isOn;
+    }
+
+    //Output the new state of the Toggle into Text
+    void ToggleValueChanged(Toggle change)
+    {
+		AudioConfiguration config = AudioSettings.GetConfiguration();
+        m_Text.text =  "New Value : " + m_Toggle.isOn;
+		if (m_Toggle.isOn) 
+		{
+            config.speakerMode = AudioSpeakerMode.Mono;
+            AudioSettings.Reset(config);
+			Debug.Log("I changed to: " + AudioSettings.speakerMode);
+		}
+		else 
+		{
+			config.speakerMode = AudioSpeakerMode.Stereo;
+			AudioSettings.Reset(config);
+			Debug.Log("I changed to: " + AudioSettings.speakerMode);
+		}
+    }
+	// Update is called once per frame
+	// void Update () {
+		
+	// }
+}
---
+++ Assets/Scripts/WalkingStick.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.iOS;
+using UnityEngine.UI;
+public class WalkingStick : MonoBehaviour {
+	public Transform camera;
+
+  	//public Text DistanceText;
+	public AudioSource audioData;
+	public Toggle stickToggle;
+	public float nextActionTime = 0.0f;
+	public float nextActionTime2 = 0.0f;
+	public float period = 0.25f;
+	public float dist = 0.0f;
+	public float bValue = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//audioData.Play(0);
+	}
+
+	 bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+    {
+        List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
+        if (hitResults.Count > 0) {
+            foreach (var hitResult in hitResults) {
+              //  Debug.Log ("purple");
+                var pos = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+				dist = Vector3.Distance(pos,camera.transform.position);
+				Debug.Log ("hey QT");
+
+				var min = 0f;
+				var max = 5f;
+
+				float normal = Mathf.InverseLerp(min, max, dist);
+				bValue = Mathf.Lerp(max, min, normal);
+				
+                return true;
+            }
+        }
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (Time.time > nextActionTime ) 
+		{ 
+			nextActionTime = Time.time + period; 
+			
+			ARPoint point = new ARPoint 
+			{
+              	x = 0.5f,
+              	y = 0.5f
+           	};
+
+           	// prioritize reults types
+           	ARHitTestResultType[] resultTypes = 
+			{
+              	ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+              	ARHitTestResultType.ARHitTestResultTypeEstimatedHorizontalPlane, 
+              	ARHitTestResultType.ARHitTestResultTypeEstimatedVerticalPlane, 
+               	ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+            }; 
+            
+            foreach (ARHitTestResultType resultType in resultTypes)
+            {
+              	if (HitTestWithResultType (point, resultType))
+               	{
+                return;
+                }
+			}
+		}
+		
+		if (stickToggle.isOn) 
+		{
+			if (Time.time > nextActionTime2)
+			{
+				nextActionTime2 = Time.time + dist/2;
+				if (audioData.isPlaying) audioData.Stop ();
+				audioData.pitch = bValue;
+				if (!audioData.isPlaying) audioData.Play(0);
+			}
+		}
+		else audioData.Stop();
+
+		//DistanceText.text = "hellooooo " + dist;
+
+    }
+}
